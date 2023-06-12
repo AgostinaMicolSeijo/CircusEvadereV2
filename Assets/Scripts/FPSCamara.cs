95c3bb7d45bc4c3fc7f77ab902ecff9e1113c42a
VendeHumo123 <agostina.seijo@davinci.edu.ar>
2023-06-12 16:43:33 -0600

    Escena

---
+++ Assets/Scripts/FPSCamara.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCamara : MonoBehaviour
+{
+    Transform camara;
+    public Vector2 sensibilidad;
+
+    //Slider
+
+
+    void Start()
+    {
+        camara = transform.Find("Camera");
+
+    }
+
+    void Update()
+    {
+        float hor = Input.GetAxis("Mouse X");
+        float ver = Input.GetAxis("Mouse Y");
+
+        if (hor != 0)
+        {
+            transform.Rotate(Vector3.up*hor*sensibilidad.x);
+        }
+        if (ver != 0)
+        {
+            //camara.Rotate(Vector3.left * ver * sensibilidad.y);
+            float angle = (camara.localEulerAngles.x - ver*sensibilidad.y + 360) % 360;
+            if (angle > 180)
+            {
+                angle -= 360;
+            }
+            angle = Mathf.Clamp(angle, -80, 80);
+            camara.localEulerAngles = Vector3.right * angle;
+        }
+    }
+}
---
+++ Assets/Scripts/MovimientoJugador.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MovimientoJugador : MonoBehaviour
+{
+    public CharacterController controler;
+    public float velocidad = 5f;
+    public float correr = 6f;
+
+    public bool estaCorriendo = false;
+
+    public float gravedad = -20f;
+    public float alturaDeSalto = 3f;
+
+    public Transform groundCheck;
+    public float distanciaPiso = 0.3f;
+    public LayerMask groundMask;
+
+    Vector3 rapidez;
+
+    bool estamosEnPasto;
+
+    float duracion = 0f;
+    float tiempoEspera = 4.5f;
+
+    public AudioSource Clip;
+
+    //[SerializeField] Animation movimiento;
+    void Start()
+    {
+        //movimiento.SetActive(false);
+    }
+
+    void Update()
+    {
+
+        estamosEnPasto = Physics.CheckSphere(groundCheck.position, distanciaPiso, groundMask);
+        if (estamosEnPasto && rapidez.y < 0)
+        {
+            rapidez.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        //move.Normalize();
+        if(x > 1 || z > 1)
+        {
+            move.Normalize();
+        }
+        controler.Move(move * velocidad * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftShift) && estaCorriendo == false)
+        {
+            controler.Move(move * correr * Time.deltaTime);
+            //movimiento.SetActive(true);
+            duracion = duracion + Time.deltaTime;
+
+            if(duracion >= 4)
+            {
+                tiempoEspera = 4.5f;
+                //print("tiempo de espera" + tiempoEspera);
+                //movimiento.SetActive(false);
+                estaCorriendo = true;
+                Clip.Play();
+            }
+        }
+        /*else
+        { cambio de mecanica
+            duracion = 0f;
+        }*/
+        if (estaCorriendo == true)
+        {
+            tiempoEspera = tiempoEspera - Time.deltaTime;
+            //print("resta" + tiempoEspera);
+            //movimiento.SetActive(false);
+            if(tiempoEspera <= 0f)
+            {
+                //movimiento.SetActive(true);
+                estaCorriendo = false;
+                duracion = 0f;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && estamosEnPasto)
+        {
+            rapidez.y = Mathf.Sqrt(alturaDeSalto * -1 * gravedad);
+        }
+
+        rapidez.y += gravedad * Time.deltaTime;
+
+        controler.Move(rapidez*Time.deltaTime);
+
+        /*
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            //movimiento.SetActive(false);
+        }*/
+    }
+}
