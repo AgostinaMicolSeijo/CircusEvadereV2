362a6a2234f8aa5ba74dddc450718c5bed37e5a8
Jasuwu <jasu-sampa@hotmail.com>
2023-07-03 22:53:48 -0300

    derrames, pero progresos

--- Assets/Scripts/IA's/Manager.cs
+++ Assets/Scripts/IA's/Manager.cs
@@ -15,19 +15,70 @@ public class Manager : MonoBehaviour
     [SerializeField] private float _rangoAudicioncorrer;
     [SerializeField] private GameObject _bolacorrer;
     [SerializeField] private GameObject _bolacaminar;
+    [SerializeField] private float _rangoVisionAumentado;
+    [SerializeField] private float _rangoVisionOriginal;
 
     //referencias
     [SerializeField] private NavMeshAgent _agent;
     [SerializeField] private Transform _targetPepito;
+    [SerializeField] private Transform _spawnN;
+    [SerializeField] private Transform _spawnR;
 
+    //array
+    [SerializeField] private Transform[] _waypoints;
+    [SerializeField] private float _offset;
+    [SerializeField] private int _arraynumber = 0;
+    Vector3 _nextWaypoint;
+
+    //timer
+    [SerializeField] private float _timer = 0;
+
+    private void Awake()
+    {
+        _nextWaypoint = _waypoints[0].position;
+        _agent.speed = 3;
+    }
 
     void Update()
     {
         if ((_bolacorrer.activeInHierarchy) && Vector3.Distance(transform.position, _targetPepito.position) <= _rangoAudicioncorrer)
         {
             transform.LookAt(_targetPepito);
+            _rangoVision = _rangoVisionAumentado;
+
         }
-        
+        else
+        {
+            StartCoroutine("Tiempo_Vision");
+            print("se activo corutina");
+        }
+        _agent.SetDestination(_nextWaypoint);
+        if (Vector3.Distance(transform.position, _nextWaypoint) <= _offset)
+        {
+
+            _arraynumber++;
+            _nextWaypoint = _waypoints[_arraynumber].position;
+            if (_arraynumber >= _waypoints.Length - 1)
+            {
+                print("paso el array el manager");
+                _arraynumber = -1;
+            }
+        }
+
+        if (Vector3.Distance(transform.position, _targetPepito.position) <= _rangoVision)
+        {
+            
+            _timer += Time.deltaTime;
+            if (_timer >= 7)
+            {
+                _timer = 0;
+            }
+            if (_timer == 0)
+            {
+              Instantiate(_prefabN, _spawnN.transform.position, _spawnN.transform.rotation);
+
+            }
+        }
     }
 
     private void OnDrawGizmos()
@@ -45,4 +96,11 @@ public class Manager : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, _rangoAudicioncorrer);
 
     }
+
+    public IEnumerator Tiempo_Vision()
+    {
+        yield return new WaitForSeconds(3f);
+        _rangoVision = _rangoVisionOriginal;
+    }
+   
 }
