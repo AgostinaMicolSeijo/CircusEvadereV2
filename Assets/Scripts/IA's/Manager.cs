ea8463a0a0e8a89d650336fe00090124e2511e22
Jasuwu <jasu-sampa@hotmail.com>
2023-07-03 21:39:25 -0300

    IA progress

--- Assets/Scripts/IA's/Manager.cs
+++ Assets/Scripts/IA's/Manager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Manager : MonoBehaviour
 {
@@ -9,15 +10,39 @@ public class Manager : MonoBehaviour
     [SerializeField] private GameObject _prefabR;
 
     //rangos
-    [SerializeField] private float _rango;
+    [SerializeField] private float _rangoVision;
+    [SerializeField] private float _rangoAudicioncaminar;
+    [SerializeField] private float _rangoAudicioncorrer;
+    [SerializeField] private GameObject _bolacorrer;
+    [SerializeField] private GameObject _bolacaminar;
 
     //referencias
+    [SerializeField] private NavMeshAgent _agent;
     [SerializeField] private Transform _targetPepito;
 
 
-    // Update is called once per frame
     void Update()
     {
-        transform.LookAt(_targetPepito);
+        if ((_bolacorrer.activeInHierarchy) && Vector3.Distance(transform.position, _targetPepito.position) <= _rangoAudicioncorrer)
+        {
+            transform.LookAt(_targetPepito);
+        }
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        //Rango al ver
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _rangoVision);
+
+        //Rango al escuchar caminar
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, _rangoAudicioncaminar);
+
+        //rango de escucha correr
+        Gizmos.color = Color.gray;
+        Gizmos.DrawWireSphere(transform.position, _rangoAudicioncorrer);
+
     }
 }
