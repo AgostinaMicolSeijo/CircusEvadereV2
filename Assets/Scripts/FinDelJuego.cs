15e9d7e4e89a77eebd39ab41c4869991932c96b8
VendeHumo123 <agostina.seijo@davinci.edu.ar>
2023-07-07 00:13:11 -0600

    fin xd

---
+++ Assets/Scripts/FinDelJuego.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinDelJuego : MonoBehaviour
+{
+    public void Salir()
+    {
+        Application.Quit();
+        print("Salir");
+    }
+
+}
